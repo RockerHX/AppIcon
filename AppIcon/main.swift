aa286387c2b43462470f4f5096549f122d4ab3b8
RockerHX <rockerhx@gmail.com>
2018-05-21 23:04:39 +0800

    Initial Commit

---
+++ AppIcon/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppIcon
+//
+//  Created by RockerHX on 2018/5/21.
+//  Copyright © 2018年 RockerHX. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
